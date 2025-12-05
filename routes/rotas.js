8f4c69c4404e62fce5621eca54e410ac0abef11b
Luis Eduardo <luisantunes.pf025@academico.ifsul.edu.br>
2025-12-05 09:45:05 -0300

    ver 2.0

--- routes/rotas.js
+++ routes/rotas.js
@@ -3,21 +3,14 @@ const { Router } = require('express');
 const { rotasCozinheiros }= require('./rotasCozinheiros');
 const { rotasIngredientes }= require('./rotasIngredientes');
 const { rotasReceitas }= require('./rotasReceitas');
-const { getUsuario, atualizaUsuario } = require('../controllers/usuarioController');
-const { verificaJWT } = require('../controllers/segurancaController');
+const { login } = require('../controllers/segurancaController');
 
 const rotas = new Router();
 
 rotas.use(rotasCozinheiros);
 rotas.use(rotasIngredientes);
 rotas.use(rotasReceitas);
-
 rotas.route("/login")
-   .post(login) 
-rotas.route("/cadastro") // rota para cadastro de usuário
-   .post(cadastro)
-rotas.route("/usuario")
-   .get(verificaJWT, getUsuario)
-   .put(verificaJWT, atualizaUsuario);
+   .post(login)  
 
-module.exports = { rotas };
+module.exports = rotas;
